68ae10a006bfdffbca808a4ec886d41ff5ac50b4
dongwlin <dongwlin@foxmail.com>
2024-07-04 01:17:34 +0800

    chore: add Rect Type

--- buffer_rect.go
+++ buffer_rect.go
@@ -7,14 +7,49 @@ package maa
 import "C"
 import "unsafe"
 
+type Rect [4]int32
+
+func (r *Rect) GetX() int32 {
+	return r[0]
+}
+
+func (r *Rect) GetY() int32 {
+	return r[1]
+}
+
+func (r *Rect) GetW() int32 {
+	return r[2]
+}
+
+func (r *Rect) GetH() int32 {
+	return r[3]
+}
+
+func (r *Rect) SetX(value int32) {
+	r[0] = value
+}
+
+func (r *Rect) SetY(value int32) {
+	r[1] = value
+}
+
+func (r *Rect) SetW(value int32) {
+	r[2] = value
+}
+
+func (r *Rect) SetH(value int32) {
+	r[3] = value
+}
+
 type RectBuffer interface {
 	Destroy()
 	Handle() unsafe.Pointer
+	Get() Rect
 	GetX() int32
 	GetY() int32
 	GetW() int32
 	GetH() int32
-	Set(x, y, w, h int32) bool
+	Set(rect Rect) bool
 	setX(value int32) bool
 	SetY(value int32) bool
 	SetW(value int32) bool
@@ -38,6 +73,10 @@ func (r *rectBuffer) Handle() unsafe.Pointer {
 	return unsafe.Pointer(r.handle)
 }
 
+func (r *rectBuffer) Get() Rect {
+	return Rect{r.GetX(), r.GetY(), r.GetW(), r.GetH()}
+}
+
 func (r *rectBuffer) GetX() int32 {
 	return int32(C.MaaGetRectX(r.handle))
 }
@@ -54,8 +93,14 @@ func (r *rectBuffer) GetH() int32 {
 	return int32(C.MaaGetRectH(r.handle))
 }
 
-func (r *rectBuffer) Set(x, y, w, h int32) bool {
-	return C.MaaSetRect(r.handle, C.int32_t(x), C.int32_t(y), C.int32_t(w), C.int32_t(h)) != 0
+func (r *rectBuffer) Set(rect Rect) bool {
+	return C.MaaSetRect(
+		r.handle,
+		C.int32_t(rect.GetX()),
+		C.int32_t(rect.GetY()),
+		C.int32_t(rect.GetW()),
+		C.int32_t(rect.GetH()),
+	) != 0
 }
 
 func (r *rectBuffer) setX(value int32) bool {
