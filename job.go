02b0c6979bae161b04c9edccd63e2ac7d59c2cf6
dongwlin <dongwlin@foxmail.com>
2024-08-27 09:47:22 +0800

    refactor: move TaskJob to job.go

--- job.go
+++ job.go
@@ -48,3 +48,24 @@ func (job Job) Wait() bool {
 	}
 	return job.Success()
 }
+
+type TaskJob struct {
+	Job
+	setParamFunc func(id int64, param string) bool
+}
+
+func NewTaskJob(id int64, statusFunc func(id int64) Status, setParamFunc func(id int64, param string) bool) TaskJob {
+	job := NewJob(id, statusFunc)
+	return TaskJob{
+		Job:          job,
+		setParamFunc: setParamFunc,
+	}
+}
+
+func (job TaskJob) SetParam(param string) bool {
+	return job.setParamFunc(job.id, param)
+}
+
+func (job TaskJob) GetDetail() (TaskDetail, bool) {
+	return QueryTaskDetail(job.id)
+}
