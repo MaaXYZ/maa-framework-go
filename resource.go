dfaa99cb78a765fa5f8dba63d220ff0e5dd4af64
dongwlin <dongwlin@foxmail.com>
2024-09-07 12:03:40 +0800

    refactor: resource

--- resource.go
+++ resource.go
@@ -4,11 +4,37 @@ package maa
 #include <stdlib.h>
 #include <MaaFramework/MaaAPI.h>
 
-extern void _MaaAPICallbackAgent(MaaStringView msg, MaaStringView detailsJson, MaaTransparentArg callbackArg);
+extern void _MaaNotificationCallbackAgent(const char* message, const char* details_json, void* callback_arg);
+
+typedef struct MaaContext* MaaContextHandle;
+
+typedef struct MaaRect* MaaRectHandle;
+
+typedef struct MaaStringBuffer* MaaStringBufferHandle;
+
+extern uint8_t _MaaCustomRecognizerCallback(
+			MaaContextHandle ctx,
+			int64_t task_id,
+            const char* recognizer_name,
+            const char* custom_recognition_param,
+            const MaaImageBufferHandle image,
+            void* recognizer_arg,
+           	MaaRectHandle out_box,
+			MaaStringBufferHandle out_detail);
+
+extern uint8_t _MaaCustomActionCallback(
+	MaaContextHandle ctx,
+	int64_t task_id,
+	const char*  task_name,
+	const char*  customActionParam,
+	MaaRectHandle box ,
+	const char* recognition_detail,
+	void* actionArg);
 */
 import "C"
 import (
 	"github.com/MaaXYZ/maa-framework-go/buffer"
+	"image"
 	"strings"
 	"unsafe"
 )
@@ -19,12 +45,12 @@ type Resource struct {
 
 // NewResource creates a new resource.
 func NewResource(callback func(msg, detailsJson string)) *Resource {
-	id := registerCallback(callback)
+	id := registerNotificationCallback(callback)
 	handle := C.MaaResourceCreate(
-		C.MaaAPICallback(C._MaaAPICallbackAgent),
+		C.MaaNotificationCallback(C._MaaNotificationCallbackAgent),
 		// Here, we are simply passing the uint64 value as a pointer
 		// and will not actually dereference this pointer.
-		C.MaaTransparentArg(unsafe.Pointer(uintptr(id))),
+		unsafe.Pointer(uintptr(id)),
 	)
 	if handle == nil {
 		return nil
@@ -39,13 +65,93 @@ func (r *Resource) Destroy() {
 	C.MaaResourceDestroy(r.handle)
 }
 
+// RegisterCustomRecognizer registers a custom recognizer to the resource.
+func (r *Resource) RegisterCustomRecognizer(
+	name string,
+	recognizer func(ctx *Context, taskId int64, recognizerName, customRecognitionParam string, img image.Image) (AnalyzeResult, bool),
+) bool {
+	id := registerCustomRecognizer(name, recognizer)
+
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	got := C.MaaResourceRegisterCustomRecognizer(
+		r.handle,
+		cName,
+		C.MaaCustomRecognizerCallback(C._MaaCustomRecognizerCallback),
+		// Here, we are simply passing the uint64 value as a pointer
+		// and will not actually dereference this pointer.
+		unsafe.Pointer(uintptr(id)),
+	)
+	return got != 0
+}
+
+// UnregisterCustomRecognizer unregisters a custom recognizer from the resource.
+func (r *Resource) UnregisterCustomRecognizer(name string) bool {
+	unregisterCustomRecognizer(name)
+
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	got := C.MaaResourceUnregisterCustomRecognizer(r.handle, cName)
+	return got != 0
+}
+
+// ClearCustomRecognizer clears all custom recognizers registered from the resource.
+func (r *Resource) ClearCustomRecognizer() bool {
+	clearCustomRecognizer()
+
+	got := C.MaaResourceClearCustomRecognizer(r.handle)
+	return got != 0
+}
+
+// RegisterCustomAction registers a custom action to the resource.
+func (r *Resource) RegisterCustomAction(
+	name string,
+	action func(ctx *Context, taskId int64, actionName, customActionParam string, box Rect, recognitionDetail string) bool,
+) bool {
+	id := registerCustomAction(name, action)
+
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	got := C.MaaResourceRegisterCustomAction(
+		r.handle,
+		cName,
+		C.MaaCustomActionCallback(C._MaaCustomActionCallbackAgent),
+		// Here, we are simply passing the uint64 value as a pointer
+		// and will not actually dereference this pointer.
+		unsafe.Pointer(uintptr(id)),
+	)
+	return got != 0
+}
+
+// UnregisterCustomAction unregisters a custom action from the resource.
+func (r *Resource) UnregisterCustomAction(name string) bool {
+	unregisterCustomAction(name)
+
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	got := C.MaaResourceUnregisterCustomAction(r.handle, cName)
+	return got != 0
+}
+
+// ClearCustomAction clears all custom actions registered from the resource.
+func (r *Resource) ClearCustomAction() bool {
+	clearCustomAction()
+
+	got := C.MaaResourceClearCustomAction(r.handle)
+	return got != 0
+}
+
 // PostPath adds a path to the resource loading paths.
 // Return id of the resource.
 func (r *Resource) PostPath(path string) Job {
 	cPath := C.CString(path)
 	defer C.free(unsafe.Pointer(cPath))
 	id := int64(C.MaaResourcePostPath(r.handle, cPath))
-	return NewJob(id, r.status)
+	return NewJob(id, r.status, r.wait)
 }
 
 // Clear clears the resource loading paths.
@@ -56,6 +162,10 @@ func (r *Resource) Clear() bool {
 // status returns the loading status of a resource identified by id.
 func (r *Resource) status(resId int64) Status {
 	return Status(C.MaaResourceStatus(r.handle, C.int64_t(resId)))
+}
+
+func (r *Resource) wait(resId int64) Status {
+	return Status(C.MaaResourceWait(r.handle, C.int64_t(resId)))
 }
 
 // Loaded checks if resources are loaded.
