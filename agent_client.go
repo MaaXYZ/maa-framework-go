01053ce3f16872749cae9af28914c5b3d69ac1da
dongwlin <dongwlin@foxmail.com>
2025-04-07 23:57:18 +0800

    feat: add doc comments to agent client methods

--- agent_client.go
+++ agent_client.go
@@ -9,6 +9,7 @@ type AgentClient struct {
 	handle uintptr
 }
 
+// NewAgentClient creates and initializes a new Agent client instance
 func NewAgentClient() *AgentClient {
 	handle := maa.MaaAgentClientCreate()
 	if handle == 0 {
@@ -20,14 +21,17 @@ func NewAgentClient() *AgentClient {
 	}
 }
 
+// Destroy releases underlying resources
 func (ac *AgentClient) Destroy() {
 	maa.MaaAgentClientDestroy(ac.handle)
 }
 
+// BindResource binds a resource object to the client
 func (ac *AgentClient) BindResource(res *Resource) bool {
 	return maa.MaaAgentClientBindResource(ac.handle, res.handle)
 }
 
+// CreateSocket creates a socket connection with specified identifier
 func (ac *AgentClient) CreateSocket(identifier string) bool {
 	identifierStrBuf := buffer.NewStringBuffer()
 	defer identifierStrBuf.Destroy()
@@ -35,10 +39,12 @@ func (ac *AgentClient) CreateSocket(identifier string) bool {
 	return maa.MaaAgentClientCreateSocket(ac.handle, identifierStrBuf.Handle())
 }
 
+// Connect attempts to establish connection with Agent service
 func (ac *AgentClient) Connect() bool {
 	return maa.MaaAgentClientConnect(ac.handle)
 }
 
+// Disconnect actively terminates current connection
 func (ac *AgentClient) Disconnect() bool {
 	return maa.MaaAgentClientDisconnect(ac.handle)
 }
