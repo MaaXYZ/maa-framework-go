c6424511f63c1d38d1ac42842adf7b10f51ae83b
dongwlin <dongwlin@foxmail.com>
2025-11-19 18:17:00 +0800

    chore: remove useless pi test cases

--- toolkit_test.go
+++ toolkit_test.go
@@ -20,29 +20,3 @@ func TestToolkit_FindDesktopWindows(t *testing.T) {
 	desktopWindows := FindDesktopWindows()
 	require.NotNil(t, desktopWindows)
 }
-
-type testToolKitRec struct{}
-
-func (t *testToolKitRec) Run(_ *Context, _ *CustomRecognitionArg) (*CustomRecognitionResult, bool) {
-	return &CustomRecognitionResult{}, true
-}
-
-func TestToolkit_RegisterPICustomRecognition(t *testing.T) {
-	PIRegisterCustomRecognition(0, "TestRec", &testToolKitRec{})
-}
-
-type testToolkitAct struct{}
-
-func (t testToolkitAct) Run(_ *Context, _ *CustomActionArg) bool {
-	return true
-}
-
-func TestToolkit_RegisterPICustomAction(t *testing.T) {
-	PIRegisterCustomAction(0, "TestAct", &testToolkitAct{})
-}
-
-func TestToolkit_ClearPICustom(t *testing.T) {
-	PIRegisterCustomRecognition(0, "TestRec", &testToolKitRec{})
-	PIRegisterCustomAction(0, "TestAct", &testToolkitAct{})
-	PIClearCustom(0)
-}
