b5f4a86c9f3166fc7e6ec21569c0abd6186692dd
dongwlin <dongwlin@foxmail.com>
2025-11-25 14:55:32 +0800

    docs: add comprehensive event type documentation

--- event.go
+++ event.go
@@ -38,9 +38,10 @@ func unregisterEventCallback(id uint64) {
 	eventCallbacksMutex.Unlock()
 }
 
+// EventStatus represents the current state of an event
 type EventStatus int
 
-// EventStatus
+// Event status constants
 const (
 	EventStatusUnknown EventStatus = iota
 	EventStatusStarting
@@ -48,12 +49,14 @@ const (
 	EventStatusFailed
 )
 
+// ResourceLoadingDetail contains information about resource loading events
 type ResourceLoadingDetail struct {
 	ResID uint64 `json:"res_id"`
 	Hash  string `json:"hash"`
 	Path  string `json:"path"`
 }
 
+// ControllerActionDetail contains information about controller action events
 type ControllerActionDetail struct {
 	CtrlID uint64         `json:"ctrl_id"`
 	UUID   string         `json:"uuid"`
@@ -61,6 +64,7 @@ type ControllerActionDetail struct {
 	Param  map[string]any `json:"param"`
 }
 
+// TaskerTaskDetail contains information about tasker task events
 type TaskerTaskDetail struct {
 	TaskID uint64 `json:"task_id"`
 	Entry  string `json:"entry"`
@@ -68,6 +72,7 @@ type TaskerTaskDetail struct {
 	Hash   string `json:"hash"`
 }
 
+// NodePipelineNodeDetail contains information about pipeline node events
 type NodePipelineNodeDetail struct {
 	TaskID uint64 `json:"task_id"`
 	NodeID uint64 `json:"node_id"`
@@ -75,6 +80,7 @@ type NodePipelineNodeDetail struct {
 	Focus  any    `json:"focus"`
 }
 
+// NodeRecognitionNodeDetail contains information about recognition node events
 type NodeRecognitionNodeDetail struct {
 	TaskID uint64 `json:"task_id"`
 	NodeID uint64 `json:"node_id"`
@@ -82,6 +88,7 @@ type NodeRecognitionNodeDetail struct {
 	Focus  any    `json:"focus"`
 }
 
+// NodeActionNodeDetail contains information about action node events
 type NodeActionNodeDetail struct {
 	TaskID uint64 `json:"task_id"`
 	NodeID uint64 `json:"node_id"`
@@ -89,6 +96,7 @@ type NodeActionNodeDetail struct {
 	Focus  any    `json:"focus"`
 }
 
+// NodeNextListDetail contains information about node next list events
 type NodeNextListDetail struct {
 	TaskID   uint64   `json:"task_id"`
 	Name     string   `json:"name"`
@@ -96,6 +104,7 @@ type NodeNextListDetail struct {
 	Focus    any      `json:"focus"`
 }
 
+// NodeRecognitionDetail contains information about node recognition events
 type NodeRecognitionDetail struct {
 	TaskID uint64 `json:"task_id"`
 	RecID  uint64 `json:"reco_id"`
@@ -103,6 +112,7 @@ type NodeRecognitionDetail struct {
 	Focus  any    `json:"focus"`
 }
 
+// NodeActionDetail contains information about node action events
 type NodeActionDetail struct {
 	TaskID uint64 `json:"task_id"`
 	NodeID uint64 `json:"node_id"`
