d27b7ebaf6dfffd7b1375bf66c8765c5642c2ea1
dongwlin <dongwlin@foxmail.com>
2026-02-02 11:41:51 +0800

    refactor: unexport TaskerEventSinkAdapter and ContextEventSinkAdapter

--- tasker.go
+++ tasker.go
@@ -535,13 +535,13 @@ type TaskerEventSink interface {
 	OnTaskerTask(tasker *Tasker, event EventStatus, detail TaskerTaskDetail)
 }
 
-// TaskerEventSinkAdapter is a lightweight adapter that makes it easy to register
+// taskerEventSinkAdapter is a lightweight adapter that makes it easy to register
 // a single-event handler via a callback function.
-type TaskerEventSinkAdapter struct {
+type taskerEventSinkAdapter struct {
 	onTaskerTask func(EventStatus, TaskerTaskDetail)
 }
 
-func (a *TaskerEventSinkAdapter) OnTaskerTask(tasker *Tasker, status EventStatus, detail TaskerTaskDetail) {
+func (a *taskerEventSinkAdapter) OnTaskerTask(tasker *Tasker, status EventStatus, detail TaskerTaskDetail) {
 	if a == nil || a.onTaskerTask == nil {
 		return
 	}
@@ -551,7 +551,7 @@ func (a *TaskerEventSinkAdapter) OnTaskerTask(tasker *Tasker, status EventStatus
 // OnTaskerTask registers a callback sink that only handles Tasker.Task events and returns the sink ID.
 // The sink ID can be used to remove the sink later.
 func (t *Tasker) OnTaskerTask(fn func(EventStatus, TaskerTaskDetail)) int64 {
-	sink := &TaskerEventSinkAdapter{onTaskerTask: fn}
+	sink := &taskerEventSinkAdapter{onTaskerTask: fn}
 	return t.AddSink(sink)
 }
 
@@ -564,9 +564,9 @@ type ContextEventSink interface {
 	OnNodeAction(ctx *Context, event EventStatus, detail NodeActionDetail)
 }
 
-// ContextEventSinkAdapter is a lightweight adapter that makes it easy to register
+// contextEventSinkAdapter is a lightweight adapter that makes it easy to register
 // a single-event handler via a callback function.
-type ContextEventSinkAdapter struct {
+type contextEventSinkAdapter struct {
 	onNodePipelineNode    func(*Context, EventStatus, NodePipelineNodeDetail)
 	onNodeRecognitionNode func(*Context, EventStatus, NodeRecognitionNodeDetail)
 	onNodeActionNode      func(*Context, EventStatus, NodeActionNodeDetail)
@@ -575,42 +575,42 @@ type ContextEventSinkAdapter struct {
 	onNodeAction          func(*Context, EventStatus, NodeActionDetail)
 }
 
-func (a *ContextEventSinkAdapter) OnNodePipelineNode(ctx *Context, status EventStatus, detail NodePipelineNodeDetail) {
+func (a *contextEventSinkAdapter) OnNodePipelineNode(ctx *Context, status EventStatus, detail NodePipelineNodeDetail) {
 	if a == nil || a.onNodePipelineNode == nil {
 		return
 	}
 	a.onNodePipelineNode(ctx, status, detail)
 }
 
-func (a *ContextEventSinkAdapter) OnNodeRecognitionNode(ctx *Context, status EventStatus, detail NodeRecognitionNodeDetail) {
+func (a *contextEventSinkAdapter) OnNodeRecognitionNode(ctx *Context, status EventStatus, detail NodeRecognitionNodeDetail) {
 	if a == nil || a.onNodeRecognitionNode == nil {
 		return
 	}
 	a.onNodeRecognitionNode(ctx, status, detail)
 }
 
-func (a *ContextEventSinkAdapter) OnNodeActionNode(ctx *Context, status EventStatus, detail NodeActionNodeDetail) {
+func (a *contextEventSinkAdapter) OnNodeActionNode(ctx *Context, status EventStatus, detail NodeActionNodeDetail) {
 	if a == nil || a.onNodeActionNode == nil {
 		return
 	}
 	a.onNodeActionNode(ctx, status, detail)
 }
 
-func (a *ContextEventSinkAdapter) OnNodeNextList(ctx *Context, status EventStatus, detail NodeNextListDetail) {
+func (a *contextEventSinkAdapter) OnNodeNextList(ctx *Context, status EventStatus, detail NodeNextListDetail) {
 	if a == nil || a.onNodeNextList == nil {
 		return
 	}
 	a.onNodeNextList(ctx, status, detail)
 }
 
-func (a *ContextEventSinkAdapter) OnNodeRecognition(ctx *Context, status EventStatus, detail NodeRecognitionDetail) {
+func (a *contextEventSinkAdapter) OnNodeRecognition(ctx *Context, status EventStatus, detail NodeRecognitionDetail) {
 	if a == nil || a.onNodeRecognition == nil {
 		return
 	}
 	a.onNodeRecognition(ctx, status, detail)
 }
 
-func (a *ContextEventSinkAdapter) OnNodeAction(ctx *Context, status EventStatus, detail NodeActionDetail) {
+func (a *contextEventSinkAdapter) OnNodeAction(ctx *Context, status EventStatus, detail NodeActionDetail) {
 	if a == nil || a.onNodeAction == nil {
 		return
 	}
@@ -620,41 +620,41 @@ func (a *ContextEventSinkAdapter) OnNodeAction(ctx *Context, status EventStatus,
 // OnNodePipelineNodeInContext registers a callback sink that only handles Node.PipelineNode events and returns the sink ID.
 // The sink ID can be used to remove the sink later.
 func (t *Tasker) OnNodePipelineNodeInContext(fn func(*Context, EventStatus, NodePipelineNodeDetail)) int64 {
-	sink := &ContextEventSinkAdapter{onNodePipelineNode: fn}
+	sink := &contextEventSinkAdapter{onNodePipelineNode: fn}
 	return t.AddContextSink(sink)
 }
 
 // OnNodeRecognitionNodeInContext registers a callback sink that only handles Node.RecognitionNode events and returns the sink ID.
 // The sink ID can be used to remove the sink later.
 func (t *Tasker) OnNodeRecognitionNodeInContext(fn func(*Context, EventStatus, NodeRecognitionNodeDetail)) int64 {
-	sink := &ContextEventSinkAdapter{onNodeRecognitionNode: fn}
+	sink := &contextEventSinkAdapter{onNodeRecognitionNode: fn}
 	return t.AddContextSink(sink)
 }
 
 // OnNodeActionNodeInContext registers a callback sink that only handles Node.ActionNode events and returns the sink ID.
 // The sink ID can be used to remove the sink later.
 func (t *Tasker) OnNodeActionNodeInContext(fn func(*Context, EventStatus, NodeActionNodeDetail)) int64 {
-	sink := &ContextEventSinkAdapter{onNodeActionNode: fn}
+	sink := &contextEventSinkAdapter{onNodeActionNode: fn}
 	return t.AddContextSink(sink)
 }
 
 // OnNodeNextListInContext registers a callback sink that only handles Node.NextList events and returns the sink ID.
 // The sink ID can be used to remove the sink later.
 func (t *Tasker) OnNodeNextListInContext(fn func(*Context, EventStatus, NodeNextListDetail)) int64 {
-	sink := &ContextEventSinkAdapter{onNodeNextList: fn}
+	sink := &contextEventSinkAdapter{onNodeNextList: fn}
 	return t.AddContextSink(sink)
 }
 
 // OnNodeRecognitionInContext registers a callback sink that only handles Node.Recognition events and returns the sink ID.
 // The sink ID can be used to remove the sink later.
 func (t *Tasker) OnNodeRecognitionInContext(fn func(*Context, EventStatus, NodeRecognitionDetail)) int64 {
-	sink := &ContextEventSinkAdapter{onNodeRecognition: fn}
+	sink := &contextEventSinkAdapter{onNodeRecognition: fn}
 	return t.AddContextSink(sink)
 }
 
 // OnNodeActionInContext registers a callback sink that only handles Node.Action events and returns the sink ID.
 // The sink ID can be used to remove the sink later.
 func (t *Tasker) OnNodeActionInContext(fn func(*Context, EventStatus, NodeActionDetail)) int64 {
-	sink := &ContextEventSinkAdapter{onNodeAction: fn}
+	sink := &contextEventSinkAdapter{onNodeAction: fn}
 	return t.AddContextSink(sink)
 }
